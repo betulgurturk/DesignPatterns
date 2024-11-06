b24c9314108c0e3f9b3d857803c82b673ebfc133
betulgurturk <betul__1997@hotmail.com>
2024-11-06 23:18:35 +0300

    observer and strategy pattern

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager();
+        var companyObserver = new CompanyObserver();
+        productManager.Attach(new CustomerObserver());
+        productManager.Attach(companyObserver);
+        productManager.UpdatePrice();
+
+        //Şirketlerin fiyat güncellemelerinden haberdar olmaması için listeden çıkarıldı.
+        productManager.Detach(companyObserver);
+        productManager.UpdatePrice();
+    }
+
+    public class ProductManager
+    {
+        List<Observer> priceObservers = new List<Observer>();
+
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Ürün fiyatı güncellendi.");
+            //Observerlara haber ver.
+            Notify();
+        }
+
+
+        public void Attach(Observer observer)
+        {
+            priceObservers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            priceObservers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (var item in priceObservers)
+            {
+                item.Update();
+            }
+        }
+    }
+
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            //ürünün fiyatı güncellendiğinde çalışacak kodlar. Örneğin mail atılacak veya sms gönderilecek.
+            Console.WriteLine("Müşteriler bilgilendirildi.");
+        }
+    }
+    class CompanyObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Şirketler bilgilendirildi.");
+        }
+    }
+
+
+
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager();
+
+        //2024 öncesi ürünler için hesaplama yapılır.
+        productManager.PriceCalculator = new Before2024ProductPriceCalculator();
+        productManager.CalculatePrice();
+
+        //2024 sonrası ürünler için hesaplama yapılır.
+        productManager.PriceCalculator = new After2024ProductPriceCalculator();
+        productManager.CalculatePrice();
+    }
+
+    abstract class ProductPriceCalcutatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2024ProductPriceCalculator : ProductPriceCalcutatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Kdv %18 üzerinden hesaplanmıştır.");
+        }
+    }
+    class After2024ProductPriceCalculator : ProductPriceCalcutatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Kdv %20 üzerinden hesaplanmıştır.");
+        }
+    }
+
+    class ProductManager
+    {
+        public ProductPriceCalcutatorBase PriceCalculator { get; set; }
+        public void CalculatePrice()
+        {
+            PriceCalculator.Calculate();
+        }
+    }
+
+}
